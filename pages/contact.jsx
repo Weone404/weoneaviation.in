96ec3bc8e787599c6f2e54553886b68cd4df8ab3
Sundergupta <sunder1234gupta@gmail.com>
2026-03-17 18:19:52 +0530

    fixing code for vercel contect us page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,5 @@
-import Layout from '../../components/Layout';
-import ScrollReveal from '../../components/ScrollReveal';
+import Layout from '../components/Layout';
+import ScrollReveal from '../components/ScrollReveal';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 
