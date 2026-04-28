cb5c5d8c315f5142a21e21a08c9387a0b05bca28
Sundergupta <sunder1234gupta@gmail.com>
2026-04-28 18:22:45 +0530

    adding csp

--- next.config.js
+++ next.config.js
@@ -4,6 +4,26 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "connect-src 'self' https://www.google.com https://www.google-analytics.com https://region1.google-analytics.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
+              "img-src 'self' data: https: https://images.unsplash.com",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
